385a15e920bc028ded97a7b6075038d845c1e02d
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2023-07-31 03:07:44 +0000

    Two backend proyects added

---
+++ Back End Development and APIs/Request Header Parser Microservice/index.js
@@ -0,0 +1,43 @@
+// index.js
+// where your node app starts
+
+// init project
+require("dotenv").config();
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get(
+  "/api/whoami",
+  (req, res, next) => {
+    req.ipaddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    req.language = req.headers["accept-language"];
+    req.software = req.headers["user-agent"];
+    next();
+  },
+  (req, res) => {
+    res.json({
+      ipaddress: req.ipaddress,
+      language: req.language,
+      software: req.software,
+    });
+  }
+);
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ Back End Development and APIs/Timestamp Microservice/index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get(
+  "/api",
+  (req, res, next) => {
+    const now = new Date();
+    req.unix = now.getTime();
+    req.utc = now.toUTCString();
+    next();
+  },
+  (req, res) => {
+    res.json({ unix: req.unix, utc: req.utc });
+  }
+);
+
+app.get(
+  "/api/:date",
+  (req, res, next) => {
+    const now = new Date(
+      isNaN(req.params.date) ? req.params.date : parseInt(req.params.date)
+    );
+    req.unix = now.getTime();
+    req.utc = now.toUTCString();
+    next();
+  },
+  (req, res) => {
+    if (req.utc == "Invalid Date") res.json({ error: req.utc });
+    else res.json({ unix: req.unix, utc: req.utc });
+  }
+);
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
