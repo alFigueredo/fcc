4cb2cb9d54ad98f6ca84b64fe32f82fa7f34ed41
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2023-07-31 06:11:40 +0000

    Adding docker files

--- back-end-development-and-apis/timestamp-microservice/index.js
+++ back-end-development-and-apis/timestamp-microservice/index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require("express");
 var app = express();
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -49,6 +50,6 @@ app.get(
 );
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
